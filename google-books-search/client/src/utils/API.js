380d8f5e12af7a8e591d35b1ffbd84f0c1fcfaea
bking1989 <bradley_king@me.com>
2019-03-21 16:19:02 -0700

    Still trying to get passing queries to work

--- google-books-search/client/src/utils/API.js
+++ google-books-search/client/src/utils/API.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+require("dotenv").config();
+const keys = require("./keys")
 
 export default {
     getBooks: function(query) {
-      return axios.get("/api/books", query);
+      return axios.get("/api/books", {params: { q: query, key: keys.googleBooks.api } });
     }
   };
--- google-books-search/routes/apiRoutes.js
+++ google-books-search/routes/apiRoutes.js
@@ -1,12 +1,9 @@
 const axios = require("axios");
 const router = require("express").Router();
 
-require("dotenv").config();
-const keys = require("./keys.js")
-
 router.get("/books", (req, res) => {
   axios
-    .get("https://www.googleapis.com/books/v1/volumes?", { params: { q: req.param.query, key: keys.googleBooks.api } })
+    .get("https://www.googleapis.com/books/v1/volumes?", { params: req.query })
     .then(data => res.json(data))
     .catch(err => console.log(err));
 });
