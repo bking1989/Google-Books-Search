8f01633bd1b1abe96fabcdc91e7ce26fa7d1157a
bking1989 <bradley_king@me.com>
2019-03-21 15:32:33 -0700

    Missed a file

--- google-books-search/routes/apiRoutes.js
+++ google-books-search/routes/apiRoutes.js
@@ -6,7 +6,7 @@ const keys = require("./keys.js")
 
 router.get("/books", (req, res) => {
   axios
-    .get("https://www.googleapis.com/books/v1/volumes?", { params: { key: keys.googleBooks.api } })
+    .get("https://www.googleapis.com/books/v1/volumes?", { params: { q: req.param.query, key: keys.googleBooks.api } })
     .then(data => res.json(data))
     .catch(err => console.log(err));
 });
