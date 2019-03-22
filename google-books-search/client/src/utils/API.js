bf7f1fc09d685eff7aa31b7dd95f1d2d06d7d1c5
bking1989 <bradley_king@me.com>
2019-03-22 10:27:15 -0700

    Got the API key to pass through, but not the query

--- google-books-search/client/src/utils/API.js
+++ google-books-search/client/src/utils/API.js
@@ -1,9 +1,7 @@
 import axios from "axios";
-require("dotenv").config();
-const keys = require("./keys")
 
 export default {
     getBooks: function(query) {
-      return axios.get("/api/books", {params: { q: query, key: keys.googleBooks.api } });
+      return axios.get("/api/books", {params: { q: query } });
     }
   };
--- google-books-search/routes/apiRoutes.js
+++ google-books-search/routes/apiRoutes.js
@@ -1,9 +1,11 @@
 const axios = require("axios");
 const router = require("express").Router();
+require("dotenv").config();
+const keys = require("./keys")
 
 router.get("/books", (req, res) => {
   axios
-    .get("https://www.googleapis.com/books/v1/volumes?", { params: req.query })
+    .get("https://www.googleapis.com/books/v1/volumes?", { params: { q: req.query, key: keys.googleBooks.api } })
     .then(data => res.json(data))
     .catch(err => console.log(err));
 });
