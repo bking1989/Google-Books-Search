9b7149f499eed12781671edd37d3359a49822cd8
bking1989 <bradley_king@me.com>
2019-03-24 11:57:27 -0700

    Trying to create a function to pass list item's props to MongoDB

--- google-books-search/client/src/components/BooksList.js
+++ google-books-search/client/src/components/BooksList.js
@@ -1,8 +1,23 @@
 import React from "react";
+import API from "../utils/API";
 
 export function BooksList({ children }) {
     return <ul className="list-group">{children}</ul>;
 }
+
+export function saveBook() {
+    let newSave = {
+        title: {title}.toString(),
+        authors: {authors}.toString(),
+        description: {description}.toString(),
+        image: {image}.toString(),
+        url: {url}.toString()
+    };
+
+    API
+    .saveBook(newSave)
+    .catch(err => console.log(err))
+};
 
 export function BooksListItem({
     authors,
@@ -24,7 +39,7 @@ export function BooksListItem({
                 </div>
                 <div className="col-sm-2 text-center">
                     <a href={url} className="btn btn-primary text-white mb-2" target="_blank" rel="noopener noreferrer">Check It Out</a>
-                    <button type="button" className="btn btn-primary saveBtn">Save Book</button>
+                    <button type="button" className="btn btn-primary saveBtn" onClick={saveBook()}>Save Book</button>
                 </div>
             </div>
         </li>
--- google-books-search/server.js
+++ google-books-search/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path");
 const mongoose = require("mongoose");
 const apiRoutes = require("./routes/apiRoutes");
 const PORT = process.env.PORT || 3001;
